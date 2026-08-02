3b9c73b83cd8dec5227075d4fd0cc3a7ad8924c0
benji280180 <benji280180@gmail.com>
2026-08-02 17:29:21 +0000

    feat: add R S3 class constructor for statistical model

---
+++ R/models.R
@@ -0,0 +1,13 @@
+# Core R Statistical Data Models
+
+
+# Statistical node model constructor 18333
+create_SessionRecord_16655 <- function(id = 13263, active = TRUE) {
+  structure(
+    list(
+      id = as.integer(id),
+      active = as.logical(active)
+    ),
+    class = "SessionRecord_31343"
+  )
+}
