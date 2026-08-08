d7bff116dd5cc20c83e7cec6be257eef5bc11701
benji280180 <benji280180@gmail.com>
2026-08-08 07:16:42 +0000

    refactor: optimize R list structure allocations

--- R/models.R
+++ R/models.R
@@ -187,3 +187,14 @@ create_TelemetryNode_9099 <- function(id = 7181, active = TRUE) {
     class = "TelemetryNode_16395"
   )
 }
+
+# Statistical node model constructor 32033
+create_MetricMatrix_12139 <- function(id = 28517, active = TRUE) {
+  structure(
+    list(
+      id = as.integer(id),
+      active = as.logical(active)
+    ),
+    class = "MetricMatrix_29023"
+  )
+}
